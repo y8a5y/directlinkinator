61eaaba95aea22e1c4c994be2cef3f6b0d12e6ef
mvvtf <code@marius.wtf>
2025-07-22 13:08:01 +0200

    add error handling + basic logging

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM python:3.13-alpine
 
 WORKDIR /usr/src/app
 
+RUN apk add ffmpeg && \
+	wget -P /usr/bin https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp && \
+	chmod a+rx /usr/bin/yt-dlp
+
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
-import os, re, dotenv, discord
+import os, re, dotenv, discord, validators, datetime
 from discord import option, ApplicationContext as Context, Message
 from subprocess import run, PIPE
+from sys import stderr
 
 intents = discord.Intents.default()
 intents.message_content = (True)
@@ -10,19 +11,34 @@ bot = discord.Bot(intents=intents)
 @bot.command(description="Tries to download and send the original video from the given url")
 @discord.option("url", type=str, description="the url to download from")
 async def download(ctx: discord.ApplicationContext, url: str):
+	at = datetime.datetime.now().isoformat(timespec="seconds")
+	by = ctx.author.name
+	base_log = "# at {} user {} used /download url={}".format(at, by, url)
+
+	if not validators.url(url):
+		error_msg = "Sorry, not a valid url."
+		print(base_log + "\nerror_msg: " + error_msg, file=stderr)
+		await ctx.respond(error_msg)
+		return
+
 	await ctx.defer()
 
-	filesize_run = run(["yt-dlp", "-f", "bv[filesize<8M]+ba[filesize<2M]", "-O", "%(filesize,filesize_approx)s", url], stdout=PIPE, stderr=PIPE)
-	if filesize_run.returncode != 0 or int(filesize_run.stdout.decode().strip()) > 10_000_000: # >10Mo
-		await ctx.respond("Sorry, the given video seems to be too big (>10Mo).", ephemeral=True)
+#	filesize_run = run(["yt-dlp", "-f", "bv[filesize<8M]+ba[filesize<2M]", "-O", "%(filesize,filesize_approx)s", url], stdout=PIPE)
+#	if filesize_run.returncode != 0 or int(filesize_run.stdout.decode().strip()) > 10_000_000: # >10Mo
+#		await ctx.respond("Sorry, the given video seems to be too big (>10Mo).", ephemeral=True)
+#		return
+
+	command = "yt-dlp -f bv[filesize<8M]+ba[filesize<2M] --print after_move:filepath --force-overwrites " + url
+	filepath_run = run(command.split(), stdout=PIPE, stderr=PIPE)
+	if filepath_run.returncode != 0:
+		error_msg = "Sorry, something went wrong. Video may be too big (>10Mo)."
+		print(base_log + "\nerror_msg: {}\ncommand: {}\ncommand.stderr:\n{}".format(error_msg, command, filepath_run.stderr.decode()), file=stderr)
+		await ctx.respond(error_msg)
 		return
 
-	filepath_run = run(["yt-dlp", "-f", "bv[filesize<8M]+ba[filesize<2M]", "--print", "after_move:filepath", "--force-overwrites", url], stdout=PIPE, stderr=PIPE)
-	if filepath_run.returncode != 0:
-		await ctx.respond("Sorry, something went wrong.", ephemeral=True)
-		return
 	filepath = filepath_run.stdout.decode().strip()
-	await ctx.respond(file=discord.File(filepath))
+	print(base_log)
+	await ctx.send_followup(file=discord.File(filepath))
 	if os.path.exists(filepath):
 		os.remove(filepath)
 
